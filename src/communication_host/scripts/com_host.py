5be81e5c568973a4b5682a55ca19f18122862599
13928584687 <1250853776@qq.com>
2021-03-04 17:39:49 +0800

    u

--- src/communication_host/scripts/com_host.py
+++ src/communication_host/scripts/com_host.py
@@ -4,6 +4,7 @@ import rospy
 import cv2
 import os
 import json
+from communication_host.srv import *
 class ComHostNode:
     def __init__(self):
     
@@ -11,32 +12,16 @@ class ComHostNode:
         
         self.rate = rospy.Rate(20)
 
-        self.cap = cv2.VideoCapture(14)  #临时调试用
+        self.cap = cv2.VideoCapture(rospy.get_param("/cam_index"))  #临时调试用
         
-        #self.cap.set(3,480) #调整相机画幅大小，上位机不可用
-        #self.cap.set(4,640)
-        self.packagePath = rospy.get_param("/pkg_path/vision")
-        self.yolov5Module = detectImage(os.path.join(self.packagePath, 'scripts/v5l-last.pt'))  #加载模型
-        rospy.Service('/vision_service',VisionDetectService, self.Callback)  #建立服务
-
-        # rospy.wait_for_service('vision_service')
-
-        # self.vision_service = rospy.ServiceProxy('vision_service',VisionService)
-
-    def create_vision_detect_service_response(self):
-        tpm = VisionDetectServiceResponse()
-        tpm.isFind = 0
-        tpm.detect_res = 0
-        tpm.conf = 0
-        self.vision_detect_service_res = tpm
+        rospy.Service('/image_trans',image_trans, self.Callback)  #建立服务
 
     def Callback(self, data):
-        self.create_vision_detect_service_response()
-        _, self.frame = self.cap.read()
-        self.vision_detect_service_res = self.yolov5Module.detect(self.frame,self.vision_detect_service_res) #传入图片以及平面图像坐标系下的点击位置，识别
-        #data-base
-        
-        return self.vision_detect_service_res
+        _,frame = self.cap.read()
+        encoded_image = cv2.imencode(".jpg", frame)[1]
+        res = image_transResponse()
+        res.img = encoded_image.flatten()
+        return res
         
     def MainLoop(self):
         while not rospy.is_shutdown():
@@ -44,5 +29,5 @@ class ComHostNode:
             
             
 if __name__ == '__main__':
-    visionNode = VisionNode()
-    visionNode.MainLoop()
+    ComHost_Node = ComHostNode()
+    ComHost_Node.MainLoop()
